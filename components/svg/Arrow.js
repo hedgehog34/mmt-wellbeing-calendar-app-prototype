853bf2281bd9793d499a5a7c463e49b79fa78fad
robert jezyk <robert.jezyk@mmtdigital.co.uk>
2021-01-13 11:27:36 +0000

    tackling iOS arrow rotation problem

--- components/svg/Arrow.js
+++ components/svg/Arrow.js
@@ -1,11 +1,7 @@
 import React from 'react'
 
-const Arrow = ({ flipHorizontal = false }) => (
-  <svg xmlns='http://www.w3.org/2000/svg' width='46.145' height='53.165' viewBox='0 0 46.145 53.165' transform={flipHorizontal ? 'rotate(180.01)' : ''}>
+export const Arrow = () => (
+  <svg xmlns='http://www.w3.org/2000/svg' width='46.145' height='53.165' viewBox='0 0 46.145 53.165'>
     <path d='M26.581,46.114l-.041.031L0,25.934l5.307-4.041L22.521,35V0h7.506V35.472L47.858,21.893l5.307,4.041L26.625,46.145Z' transform='translate(0 53.165) rotate(-90)'/>
   </svg>
 )
-
-export const ArrowRight = () => <Arrow />
-
-export const ArrowLeft = () => <Arrow flipHorizontal />
