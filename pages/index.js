c93f48693cccdb9a9b2f076158c3e4446cade59f
robert jezyk <robert.jezyk@mmtdigital.co.uk>
2021-01-29 09:05:03 +0000

    home page adjustemnts

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,37 @@
+import { Fragment } from 'react'
 import Head from 'next/head'
 import Link from 'next/link'
-import { Heading, Grid, Square } from '@chakra-ui/react'
+import { Heading, Grid, Square, Box, Text } from '@chakra-ui/react'
 import { motion } from 'framer-motion'
 
-import { getAllMonthNames } from '../lib/months'
+import { getAllMonthsData } from '../lib/months'
 
 import Layout, { siteTitle } from '../components/layout'
+import { CardContent } from '../components/CardContent'
+
+const cardContainerStyles = {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: { base: 'center', md: 'initial' },
+  justifyContent: { base: 'center', md: 'initial' },
+  flexShrink: 0,
+  flexGrow: 0,
+  textOverflow: 'ellipsis',
+  whiteSpace: 'initial',
+  overflow: 'visible',
+  width: '100%',
+  height: '100%',
+  minHeight: '200px',
+  background: '#ffffff',
+}
 
 const cardHeaderStyling = {
   as: 'h3',
+  pt: { base: 0, md: 2 },
   w: '100%',
   size: '3xl',
   textTransform: 'uppercase',
-  textAlign: 'center',
+  textAlign: { base: 'center', md: 'left' },
   position: 'relative',
   zIndex: 2,
   _after: {
@@ -28,17 +47,22 @@ const cardHeaderStyling = {
   }
 }
 
+const cardContainerDescriptionStyles = {
+  display: { base: 'none', md: 'block' },
+  p: 4,
+}
+
 const gridStyles = {
-  gap: 5,
+  gap: { base: 5, md: 7 },
+  gridAutoRows: '1fr',
   templateColumns: {
     base: 'repeat(1, 1fr)',
     sm: 'repeat(2, 1fr)',
     md: 'repeat(4, 1fr)',
-    lg: 'repeat(6, 1fr)',
   }
 }
 
-export default function Home({ allMonthNames }) {
+export default function Home({ allMonthsData }) {
   return (
     <Layout home>
       <Head>
@@ -56,17 +80,20 @@ export default function Home({ allMonthNames }) {
       }}>
         <main>
           <Grid {...gridStyles}>
-            {allMonthNames.map(({ name }, index) => {
+            {allMonthsData.map(({ name, events }, index) => {
               const truncatedTitle = `${name.substring(0, 3)}.`
 
               return (
                 <Link href={`/months/${name}`} key={`${name}-${index}`}>
                   <a>
-                    <Square w='100%' h='200px' bg='white' overflow='visible' isTruncated>
-                      <Heading {...cardHeaderStyling}>
-                        {truncatedTitle}
+                    <Box {...cardContainerStyles} isTruncated>
+                      <Heading {...cardHeaderStyling} _before={{ content: { base: `'${truncatedTitle}'`, xl: `'${name}'` }}}>
+                        &nbsp;
                       </Heading>
-                    </Square>
+                      <Box {...cardContainerDescriptionStyles}>
+                        <CardContent events={events} fontSize='lg' />
+                      </Box>
+                    </Box>
                   </a>
                 </Link>
               )
@@ -83,10 +110,10 @@ export default function Home({ allMonthNames }) {
 }
 
 export async function getStaticProps({ preview = false }) {
-  const allMonthNames = await getAllMonthNames(preview)
+  const allMonthsData= await getAllMonthsData(preview)
   return {
     props: {
-      allMonthNames
+      allMonthsData,
     }
   }
 }
